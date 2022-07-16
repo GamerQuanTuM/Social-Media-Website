dd46e8f4c2bd7c7c8d2eec64c57da3dac8fa2788
Shuvam <shuvamsantra10@gmail.com>
2022-07-16 12:38:00 +0530

    Authentication added using jsonwebtoken

--- Controllers/AuthController.js
+++ Controllers/AuthController.js
@@ -1,26 +1,39 @@
 import UserModel from "../Models/userModel.js";
 import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
 
 //Registering a new user
 export const registerUser = async (req, res) => {
-    const { username, password, firstname, lastname } = req.body
+    // const { username, password, firstname, lastname } = req.body
 
     //Hashing Password for security
     const salt = await bcrypt.genSalt(10)
-    const hashedPassword = await bcrypt.hash(password, salt)
+    const hashedPassword = await bcrypt.hash(req.body.password, salt)
+    req.body.password = hashedPassword
 
     //creating a new User for which we have to map the data of req.body in our model 
-    const newUser = new UserModel({
-        username,
-        password: hashedPassword,
-        firstname,
-        lastname
-    })
+    const newUser = new UserModel(
+        // { username,
+        // password: hashedPassword,
+        // firstname,
+        // lastname}
+        req.body
+    )
+
+    const { username } = req.body
 
     //Interacting with the server and save the new User in db
     try {
-        await newUser.save()
-        res.status(200).json(newUser)
+        const oldUser = await UserModel.findOne({ username })
+        if (oldUser) {
+            return res.status(400).json({ message: "User is already registered" })
+        }
+        const user = await newUser.save()
+        const token = jwt.sign({
+            username: user.username,
+            id: user._id
+        }, process.env.JWT_KEY, { expiresIn: '1hr' })
+        res.status(200).json({ user, token })
     } catch (error) {
         res.status(500).json({
             message: error.message
@@ -43,7 +56,15 @@ export const loginUser = async (req, res) => {
         if (user) {
             const validation = await bcrypt.compare(password, user.password)
 
-            validation ? res.status(200).json(user) : res.status(400).json("Wrong Password")
+            if (!validation) {
+                res.status(400).json("Wrong Password")
+            } else {
+                const token = jwt.sign({
+                    username: user.username,
+                    id: user._id
+                }, process.env.JWT_KEY, { expiresIn: '1hr' })
+                res.status(200).json({user,token})
+            }
         } else {
             res.status(404).json("User doesnt exists")
         }
